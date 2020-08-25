8cf61f52dd094638343743a9142bef64570d5b0e
Evelyn Nguyen <Evelyn@viets-imacx.lan>
2020-08-25 16:31:12 -0700

    mvp plus seed

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,6 @@ module.exports = {
     client: "sqlite3", 
     useNullAsDefault: true, //option that's require for SQLite
     connection: {
-        filename: "./data/car-dealer.db3", //location of our database file
+        filename: "./data/cars.db3", //location of our database file
     }
 }
